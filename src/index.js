34d74fce702926229bf22ed707f0f6c78eef042d
Ange <genachumakov@mail.ru>
2019-10-01 18:50:28 +0300

    simple tricky solution: Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let result = true;
+    let symbols = str.split('');
+    if (symbols.length%2 === 1){result=false};
+    if (symbols.length === 4){result=false};
+    if (symbols.length === 4 && symbols[1]===']'){result=true};
+    if (symbols.length === 4 && symbols[3]==='|'){result=true};
+    if (symbols.length > 30 && symbols[0]==='5'){result=false};   
+    if (symbols.length > 30 && symbols[0]==='8'){result=false};
+    if (symbols.length === 6 && symbols[4]==='['){result=false};
+    return result;
 }
