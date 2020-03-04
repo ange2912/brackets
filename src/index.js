3642fa861e8a71c46dcc6df7cd16e8efa0f2a570
Ange <genachumakov@mail.ru>
2020-03-04 21:02:08 +0300

    fix: right solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-    let result = true;
-    let symbols = str.split('');
-    if (symbols.length%2 === 1){result=false};
-    if (symbols.length === 4){result=false};
-    if (symbols.length === 4 && symbols[1]===']'){result=true};
-    if (symbols.length === 4 && symbols[3]==='|'){result=true};
-    if (symbols.length > 30 && symbols[0]==='5'){result=false};   
-    if (symbols.length > 30 && symbols[0]==='8'){result=false};
-    if (symbols.length === 6 && symbols[4]==='['){result=false};
-    return result;
+    let i=0;
+    while (i < bracketsConfig.length){
+        let currentBracket = bracketsConfig[i].join('');
+        if (str.includes(currentBracket)){
+            str = str.replace(currentBracket,'');
+            i=0;
+        }else{
+            i++;
+        }
+    }
+    return str ? false : true;
 }
